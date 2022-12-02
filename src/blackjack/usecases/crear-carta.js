23f0e7c0e3859c28193d7a746921fcf3d825c97a
Allan <al.silvaquiroz@gmail.com>
2022-12-01 22:51:40 -0300

    docs updated

--- src/blackjack/usecases/crear-carta.js
+++ src/blackjack/usecases/crear-carta.js
@@ -7,7 +7,7 @@
 export const crearCarta = (carta, turno, divCartasJugadores) => {
 
     const imgCarta = document.createElement('img');
-    imgCarta.src = `/assets/img/${carta}.png`;
+    imgCarta.src = `/blackjack-vite/assets/img/${carta}.png`;
     imgCarta.classList.add('carta');
     divCartasJugadores[turno].append(imgCarta);
 
